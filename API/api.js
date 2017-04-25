d43284ff76f0d23fe80ca4f53b9f263b4d13096e
Leiver Andres Campeon <Leiver Andres Campeon>
2017-04-24 23:26:56 -0500

    Minor changes and add first user model

--- API/api.js
+++ API/api.js
@@ -1,8 +1,18 @@
 const restify = require('restify');
+const mongoose = require('mongoose');
+require('colors');
 
 const env = process.env.NODE_ENV || 'development';
 
-const server = restify.createServer({
+const api = {};
+
+api.server = restify.createServer({
   name: 'turismoEjecafeteroAPI',
   version: '1.0.0',
+});
+
+api.db = mongoose.createConnection('mongodb://127.0.0.1:27017/turismo');
+
+api.db.on('open', () => {
+  console.log('Connected to DB'.yellow);
 });
