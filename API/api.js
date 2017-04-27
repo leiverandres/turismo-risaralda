6ec81d1c8625b5c26af7287486e87184574e2cef
Leiver Andres Campeon <Leiver Andres Campeon>
2017-04-27 14:10:55 -0500

    Some changes in api main file

--- API/api.js
+++ API/api.js
@@ -1,18 +1,30 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
+const logger = require('morgan');
 require('colors');
 
 const env = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 8080;
+const config = require(`./config/${env}`);
 
 const api = {};
 
 api.server = restify.createServer({
   name: 'turismoEjecafeteroAPI',
-  version: '1.0.0',
+  version: '1.0.0'
 });
 
-api.db = mongoose.createConnection('mongodb://127.0.0.1:27017/turismo');
+api.server.use(logger('dev'));
+api.server.use(restify.acceptParser(api.server.acceptable));
+api.server.use(restify.queryParser());
+api.server.use(restify.bodyParser());
 
-api.db.on('open', () => {
-  console.log('Connected to DB'.yellow);
-});
+const usersRoutes = require('./routes/users');
+const channelsRoutes = require('./routes/channels');
+
+api.db = require('./models/db');
+
+usersRoutes(api, '/api/users');
+channelsRoutes(api, '/api/channels');
+
+module.exports = api;
