47c7ad4e2934fb1eb50ae0f865563d7a9a7da828
Leiver Andres Campeon <Leiver Andres Campeon>
2017-04-27 14:02:15 -0500

    Add models resolves #1

---
+++ API/models/root.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+const rootSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+});
+
+rootSchema.pre('save', function(next) {
+  let root = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(root.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        root.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+rootSchema.methods.comparePassword = function(candidatePassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(isMatch);
+    });
+  });
+};
+
+module.exports = mongoose.model('Root', rootSchema);
--- API/models/user.js
+++ API/models/user.js
@@ -4,47 +4,99 @@ const userSchema = new Schema({
   dni: {
     type: String,
     unique: true,
-    require: true,
+    require: true
   },
   firstName: {
     type: String,
-    require: true,
+    require: true
   },
   lastName: {
     type: String,
-    require: true,
+    require: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    require: true
   },
   email: {
     type: String,
     unique: true,
-    require,
+    require: true
   },
   address: {
-    type: String,
+    type: String
   },
   numbers: {
-    type: [String],
+    type: [String]
   },
   country: {
-    type: String,
+    type: String
   },
   city: {
-    type: String,
+    type: String
   },
   password: {
     type: String,
-    require: true,
+    require: true
   },
   age: {
-    type: Number,
+    type: Number
   },
   gender: {
     type: String,
     enum: ['male', 'female'],
+    default: 'male'
   },
   roles: {
+    // May be user, admin, root
     type: [String],
+    default: ['user']
   },
+  state: {
+    type: String,
+    enum: ['accepted', 'pending'],
+    default: 'pending'
+  },
+  pendingChannels: {
+    type: Schema.ObjectId,
+    ref: 'Channel'
+  },
+  acceptedChannels: {
+    type: Schema.ObjectId,
+    ref: 'Channel'
+  }
 });
+
+userSchema.pre('save', function(next) {
+  let thisUser = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(thisUser.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        thisUser.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(isMatch);
+    });
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
